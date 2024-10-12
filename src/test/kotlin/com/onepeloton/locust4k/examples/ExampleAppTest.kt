3cb56eb514d02d0195f91c16691dea3f27ead2cb
Travis Haagen <travis.haagen@gmail.com>
2024-10-12 09:43:55 -0700

    singleWorkerMultipleUsersWithLocustMasterEndingTest

--- src/test/kotlin/com/onepeloton/locust4k/examples/ExampleAppTest.kt
+++ src/test/kotlin/com/onepeloton/locust4k/examples/ExampleAppTest.kt
@@ -9,11 +9,24 @@ import java.time.Duration
 class ExampleAppTest : TestcontainersBase() {
     @Test
     fun singleWorkerWithLocustMasterEndingTest() {
-        val locustMasterContainer = buildLocustMasterContainer()
+        val locustMasterContainer = buildLocustMasterContainer(runtimeSeconds = 5)
         val locustWorkerExampleContainer = buildLocustWorkerExampleContainer()
         try {
+            // given
             startContainers(locustMasterContainer, locustWorkerExampleContainer)
 
+            LogMessageWaitStrategy()
+                .withRegEx(".*All users spawned.*\\s")
+                .withStartupTimeout(Duration.ofSeconds(5))
+                .waitUntilReady(locustMasterContainer)
+
+            LogMessageWaitStrategy()
+                .withTimes(2)
+                .withRegEx(".*Aggregated.*\\s")
+                .withStartupTimeout(Duration.ofSeconds(5))
+                .waitUntilReady(locustMasterContainer)
+
+            // when
             LogMessageWaitStrategy()
                 .withRegEx(".* Receive Message producer ZMQ socket closed.*\\s")
                 .withStartupTimeout(Duration.ofSeconds(5))
@@ -21,6 +34,7 @@ class ExampleAppTest : TestcontainersBase() {
 
             val workerLogs = locustWorkerExampleContainer.logs
 
+            // then
             assertThat(
                 workerLogs,
                 stringContainsInOrder(
@@ -34,6 +48,65 @@ class ExampleAppTest : TestcontainersBase() {
                     "Spawning Complete",
                     "Quit message",
                     "Task (exampleTask) cancelled",
+                    "afterExecuteLoop invoked",
+                    "Stats consumer stopped",
+                    "Stats reporter stopped",
+                    "Shutting down Locust Worker",
+                    "Closing connection",
+                    "Receive Message producer ZMQ socket closed",
+                ),
+            )
+        } finally {
+            stopContainers(locustMasterContainer, locustWorkerExampleContainer)
+        }
+    }
+
+    @Test
+    fun singleWorkerMultipleUsersWithLocustMasterEndingTest() {
+        val locustMasterContainer = buildLocustMasterContainer(users = 3, spawnRate = 2, runtimeSeconds = 5)
+        val locustWorkerExampleContainer = buildLocustWorkerExampleContainer()
+        try {
+            // given
+            startContainers(locustMasterContainer, locustWorkerExampleContainer)
+
+            LogMessageWaitStrategy()
+                .withRegEx(".*All users spawned.*\\s")
+                .withStartupTimeout(Duration.ofSeconds(5))
+                .waitUntilReady(locustMasterContainer)
+
+            LogMessageWaitStrategy()
+                .withTimes(2)
+                .withRegEx(".*Aggregated.*\\s")
+                .withStartupTimeout(Duration.ofSeconds(5))
+                .waitUntilReady(locustMasterContainer)
+
+            // when
+            LogMessageWaitStrategy()
+                .withRegEx(".* Receive Message producer ZMQ socket closed.*\\s")
+                .withStartupTimeout(Duration.ofSeconds(5))
+                .waitUntilReady(locustWorkerExampleContainer)
+
+            val workerLogs = locustWorkerExampleContainer.logs
+
+            // then
+            assertThat(
+                workerLogs,
+                stringContainsInOrder(
+                    "Starting Locust Worker",
+                    "Connected to Locust",
+                    "Received ack",
+                    "Spawn message from controller",
+                    "Stats reset",
+                    "+2 (-0) user-units",
+                    "beforeExecuteLoop invoked",
+                    "beforeExecuteLoop invoked",
+                    "Spawn message from controller",
+                    "+1 (-0) user-units",
+                    "beforeExecuteLoop invoked",
+                    "Spawning Complete",
+                    "Quit message",
+                    "afterExecuteLoop invoked",
+                    "afterExecuteLoop invoked",
                     "afterExecuteLoop invoked",
                     "Stats consumer stopped",
                     "Stats reporter stopped",
